a9248a25714a2ce53b94695c527eb629d91d4342
Pauxy Gentil-Nunes <30673056+Pauxygnunes@users.noreply.github.com>
2024-04-22 07:35:23 -0300

    Update lexset.m

--- lexset.m
+++ lexset.m
@@ -1,29 +1,24 @@
 function ls = lexset(n)
-% Returns the lexset of a positive integer number, made by all partitions
-% from 1 to n.
-%
+%    The function lexset returns the lexset of a positive integer number, made by all partitions
+%    from 1 to n.
 % Usage
-%   function ls = lexset(n)
-% 
+%    function ls = lexset(n)
 % Input argument: 
-%	N = número inteiro positivo
-%
+%	N = nÃºmero inteiro positivo
 % Output: 
-%	A = Lista de partições de 1 até n.
-%
+%	A = Lista de partiÃ§Ãµes de 1 atÃ© n.
 % Example: 
 %     lexset(4)
-%
 % Observations:
 %   The lexset of a positive integer number is a set of textural
 %   configurations for musical applications (mainly compositional), as it
 %   represents the potential of use determined by the selection of a
 %   instrumental, vocal or electronic medium (see Gentil-Nunes 2009)
-%
-% Created in 2004, january, under MATLAB 6.0 (PC)
-% © Part of PARSEMAT - Parseme Toolbox Software Package, 
-% Copyright © 2003, 2005, 2007, 2009, Pauxy Gentil Nunes Filho
-% See License.txt
+% Info
+%     Created in 2004, january, under MATLAB 6.0 (PC)
+%     Â© Part of PARSEMAT - Parseme Toolbox Software Package, 
+%     Copyright Â© 2003, 2005, 2007, 2009, Pauxy Gentil Nunes Filho
+%     See License.txt
 % ========== Join all partitions from 1 to n
 tab = [];
     for f = 1:n
