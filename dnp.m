bcabf7f23421632378dca78d1c9bdecee7b4312f
Pauxy Gentil-Nunes <30673056+Pauxygnunes@users.noreply.github.com>
2024-04-22 07:23:12 -0300

    Update dnp.m

--- dnp.m
+++ dnp.m
@@ -1,27 +1,21 @@
 function d = dnp(tab)
-% The funcion dnp calculates the agglomeration indices for each part of the
-% partition table. Returns a table where each part is substituted by its
-% agglomeration index.
-%
+%   The funcion dnp calculates the agglomeration indices for each part of the
+%   partition table. Returns a table where each part is substituted by its
+%   agglomeration index.
 % Usage:
 %   tabd = dnp(tab)
-%
 % Input argument: 
-%	tab (partition table)
-%
+%	  tab (partition table)
 % Output: 
-%	tabd (agglomeration indices table)
-% 
+%	  tabd (agglomeration indices table)
 % Example:
 %     dnp(tab)
-%
 % Observations: 
 %   This function is necessary to calculate the sum of agglomeration
 %   indices for the agglomeration index of partition.
-% 
 % Created in 2004, january, under MATLAB 6.0 (PC)
-% © Part of PARSEMAT - Parseme Toolbox Software Package, 
-% Copyright © 2003, 2005, 2007, 2009, Pauxy Gentil Nunes Filho
+% Â© Part of PARSEMAT - Parseme Toolbox Software Package, 
+% Copyright Â© 2003, 2005, 2007, 2009, Pauxy Gentil Nunes Filho
 % See License.txt
 % ========== Calculation of pairwise combinations of each part 
 d = ((berry(tab)) .* ((berry(tab))-1))/2;
