9fc0b86500ea9c13a67d923c374eaa211dcf4d0b
Pauxy Gentil-Nunes <30673056+Pauxygnunes@users.noreply.github.com>
2024-04-22 07:36:31 -0300

    Update midi2nm.m

--- midi2nm.m
+++ midi2nm.m
@@ -1,26 +1,19 @@
 % Script midi2nm
-% 
-% Reading and conversion of MIDI file into a note matrix (MIDI Toolbox
-% format).
-%
-% The script opens the selection dialogue and converts the chosen file to
-% the note matrix format.
-%
-% Usage:   
-%
-%     midi2nm   
-%
-% Observations:
-%
-% Created in february 2009, under MATLAB 7.0 (PC).
-% © Part of PARSEMAT - Parseme Toolbox Software Package,    
-% Copyright © 2003, 2005, 2007, 2009, Pauxy Gentil Nunes Filho -
-% pauxygnunes@gmail.com
-% See License.txt
-% 'readmidi' and 'quantize' are functions of MIDI Toolbox Software Package.
-% by Tuomas Eerola & Petri Toiviainen, Department of Music,
-% ptee@cc.jyu.fi, ptoiviai@cc.jyu.fi 
-% Copyright © 2004, University of Jyvaskyla, Finland
+%   Reading and conversion of MIDI file into a note matrix (MIDI Toolbox
+%   format). The script opens the selection dialogue and converts the chosen file to
+%   the note matrix format.
+% Usage:
+%     midi2nm
+% Info
+%   Created in february 2009, under MATLAB 7.0 (PC).
+%   Â© Part of PARSEMAT - Parseme Toolbox Software Package,    
+%   Copyright Â© 2003, 2005, 2007, 2009, Pauxy Gentil Nunes Filho -
+%   pauxygnunes@gmail.com
+%   See License.txt
+%   'readmidi' and 'quantize' are functions of MIDI Toolbox Software Package.
+%   by Tuomas Eerola & Petri Toiviainen, Department of Music,
+%   ptee@cc.jyu.fi, ptoiviai@cc.jyu.fi 
+%   Copyright Â© 2004, University of Jyvaskyla, Finland
 % ========== Getting the file address
 [arquivo, endereco] = uigetfile({'*.mid', 'Choose MIDI'}); 
 caminhocompleto = [endereco arquivo];
