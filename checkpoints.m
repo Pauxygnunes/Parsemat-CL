6bf53efa83ba36118063239bfc2a20009f77d27e
Pauxy Gentil-Nunes <30673056+Pauxygnunes@users.noreply.github.com>
2024-04-22 07:20:58 -0300

    Update checkpoints.m

--- checkpoints.m
+++ checkpoints.m
@@ -10,20 +10,19 @@ function a = checkpoints(nm)
 % Example: 
 %     checkpoints(nm)
 % Observations: 
-% When using 'checkpoints' instead of 'attacks' in the functions
-% 'parsemarit', 'parsemachan' and 'parsemalin', it allows for the recording
-% of silences, which can be useful in some analysis situations. For
-% 'parsemarit', 'attacks' will be used as the default. For 'parsemachan',
-% it's recommended to use 'checkpoints'. 
-%
+%   When using 'checkpoints' instead of 'attacks' in the functions
+%   'parsemarit', 'parsemachan' and 'parsemalin', it allows for the recording
+%   of silences, which can be useful in some analysis situations. For
+%   'parsemarit', 'attacks' will be used as the default. For 'parsemachan',
+%   it's recommended to use 'checkpoints'. 
 % Created in 2004, january, under MATLAB 6.0 (PC)
-% © Part of PARSEMAT - Parseme Toolbox Software Package, 
-% Copyright © 2003, 2005, 2007, 2009, Pauxy Gentil Nunes Filho
+% Â© Part of PARSEMAT - Parseme Toolbox Software Package, 
+% Copyright Â© 2003, 2005, 2007, 2009, Pauxy Gentil Nunes Filho
 % See License.txt
 % 'Onset' is a function of MIDI Toolbox Software Package,
 % by Tuomas Eerola & Petri Toiviainen, Department of Music,
 % ptee@cc.jyu.fi, ptoiviai@cc.jyu.fi 
-% Copyright © 2004, University of Jyvaskyla, Finland
+% Copyright Â© 2004, University of Jyvaskyla, Finland
 % ========== Extracting checkpoints from note matrix
 noteons = onset(nm);
 noteoffs = onset(nm) + dur(nm);
