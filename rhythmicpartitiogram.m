675802d83e734f869b07ba958090b5a1b27e9a63
Pauxy Gentil-Nunes <30673056+Pauxygnunes@users.noreply.github.com>
2024-04-22 07:41:18 -0300

    Update rhythmicpartitiogram.m

--- rhythmicpartitiogram.m
+++ rhythmicpartitiogram.m
@@ -1,19 +1,17 @@
 % Script rhythmicpartitiogram
-% 
-% The script rhythmicpartitiogram provides the rhythmic partitiogram of the
-% MIDI file. The script asks for the MIDI file location, calculates and
-% render the rhythmic partitiogram.
-%
-% Exemplo: 
+%   The script rhythmicpartitiogram provides the rhythmic partitiogram of the
+%   MIDI file. The script asks for the MIDI file location, calculates and
+%   render the rhythmic partitiogram.
+% Example: 
 %     rhythmicpartitiogram
-% 
-% Observations: The rhythmic partitiogram is an inventory of all
-% partitions of MIDI file (Carvalho and Gentil-Nunes 2003)
-%
-% Created in february 2009, under MATLAB 7.0 (PC)
-% © Part of PARSEMAT - Parseme Toolbox Software Package, 
-% Copyright © 2003, 2005, 2007, 2009, Pauxy Gentil Nunes Filho
-% See License.txt
+% Observations: 
+%   The rhythmic partitiogram is an inventory of all
+%   partitions of MIDI file (Carvalho and Gentil-Nunes 2003)
+% Info
+%   Created in february 2009, under MATLAB 7.0 (PC)
+%   Â© Part of PARSEMAT - Parseme Toolbox Software Package, 
+%   Copyright Â© 2003, 2005, 2007, 2009, Pauxy Gentil Nunes Filho
+%   See License.txt
 % ========== Calculating tab from file
 midi2nm
 tab = prhythmic(nm);
