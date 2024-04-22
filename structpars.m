cd367e7c44384845f62d41e4668d2e552b655368
Pauxy Gentil-Nunes <30673056+Pauxygnunes@users.noreply.github.com>
2024-04-22 07:43:37 -0300

    Update structpars.m

--- structpars.m
+++ structpars.m
@@ -1,31 +1,26 @@
 function sp = structpars(tab)
-% 
-% The function structpars provides a list of timepoints where changes of
-% partitions occurs (structural junctions). These points delimitate the
-% parsemes, there is, the musical areas constrained by a specific
-% partition.
-% 
+%     The function structpars provides a list of timepoints where changes of
+%     partitions occurs (structural junctions). These points delimitate the
+%     parsemes, there is, the musical areas constrained by a specific
+%     partition.
 % Input argument: 
 %	tab (partition table)
-%
 % Output: 
 %	sp (structural junctions)
-%
 % Example: 
 %     structpars(tab)
-%
 % Observations:
 %   It is necessary to have the tab variable in workspace.
-%
-% Created in 2004, january, under MATLAB 6.0 (PC)
-% © Part of PARSEMAT - Parseme Toolbox Software Package, 
-% Copyright © 2003, 2005, 2007, 2009, Pauxy Gentil Nunes Filho
-% See License.txt
-% 'onset' is a function of MIDI Toolbox Software Package,
-% by Tuomas Eerola & Petri Toiviainen, Department of Music,
-% ptee@cc.jyu.fi, ptoiviai@cc.jyu.fi 
-% Copyright © 2004, University of Jyvaskyla, Finland
-% remoçao de duplicatas e definiçao das juncoes estruturais.
+% Info:
+%     Created in 2004, january, under MATLAB 6.0 (PC)
+%     Â© Part of PARSEMAT - Parseme Toolbox Software Package, 
+%     Copyright Â© 2003, 2005, 2007, 2009, Pauxy Gentil Nunes Filho
+%     See License.txt
+%     'onset' is a function of MIDI Toolbox Software Package,
+%     by Tuomas Eerola & Petri Toiviainen, Department of Music,
+%     ptee@cc.jyu.fi, ptoiviai@cc.jyu.fi 
+%     Copyright Â© 2004, University of Jyvaskyla, Finland
+%     remoÃ§ao de duplicatas e definiÃ§ao das juncoes estruturais.
 % ========== Initialization
 at = tab(:,1);
 tab2 = tab(:,2:end);
