6bbdb0774d861e7d13c97a7023c654b2a5816f2d
Pauxy Gentil-Nunes <30673056+Pauxygnunes@users.noreply.github.com>
2024-04-22 07:22:12 -0300

    Update dispers.m

--- dispers.m
+++ dispers.m
@@ -1,32 +1,24 @@
 function d = dispers(tab)
-% 
-% The function dispers provides the dispersion indices for each partition
-% of partition table.
-%
+%   The function dispers provides the dispersion indices for each partition
+%   of partition table.
 % Usage
 %   d = dispers(partition table) 
-%
 % Input argument: 
-%	tab (partition table)
-%
+%	  tab (partition table)
 % Output: 
-%	d = dispersion indices
-% 
-% Exemplo:
+%	  d = dispersion indices
+% Example:
 %     dispers(tab)
-%
 % Observations: 
 %   The dispersion index constitutes, with the agglomeration index, a pair
 %   of coordinates for plotting indexogram and partitiogram. (Gentil-Nunes
 %   2009).
-%
 % Created in 2004, january, under MATLAB 6.0 (PC)
-% © Part of PARSEMAT - Parseme Toolbox Software Package, 
-% Copyright © 2003, 2005, 2007, 2009, Pauxy Gentil Nunes Filho
+% Â© Part of PARSEMAT - Parseme Toolbox Software Package, 
+% Copyright Â© 2003, 2005, 2007, 2009, Pauxy Gentil Nunes Filho
 % See License.txt
 % =========== Dispesion index as the difference between totalrel and agglom
 % index.
 T = totalrel(tab);
 A = agglom(tab);
 d = T - A;
-
