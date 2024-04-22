dad1d482e77b9f087955db6b7cf7ffe8f42a60c3
Pauxy Gentil-Nunes <30673056+Pauxygnunes@users.noreply.github.com>
2024-04-22 07:40:41 -0300

    Update rhythmicindexogram.m

--- rhythmicindexogram.m
+++ rhythmicindexogram.m
@@ -1,18 +1,16 @@
 % Script rhythmicindexogram 
-% 
-% The script rhythimicindexogram provides, from a MIDI file, the
-% correspondent rhythmic partitioning indexogram.
-% 
+%   The script rhythimicindexogram provides, from a MIDI file, the
+%   correspondent rhythmic partitioning indexogram.
 % Example:
 %     rhythmicindexogram
-% 
-% Observations: The rhythmic partitioning is part of Partitional Analysis
-% (Gentil-Nunes 2009).
-%
-% Created in february 2009, under MATLAB 6.0 (PC)
-% © Part of PARSEMAT - Parseme Toolbox Software Package, 
-% Copyright © 2003, 2005, 2007, 2009, Pauxy Gentil Nunes Filho
-% See License.txt
+% Observations: 
+%   The rhythmic partitioning is part of Partitional Analysis
+%   (Gentil-Nunes 2009).
+% Info
+%   Created in february 2009, under MATLAB 6.0 (PC)
+%   Â© Part of PARSEMAT - Parseme Toolbox Software Package, 
+%   Copyright Â© 2003, 2005, 2007, 2009, Pauxy Gentil Nunes Filho
+%   See License.txt
 % ========== Commands for rhythmic partitioning indexogram
 midi2nm
 tab = prhythmic(nm);
