a0d15c6146c114912ba8779dfbee5c5e312ee6a4
Pauxy Gentil-Nunes <30673056+Pauxygnunes@users.noreply.github.com>
2024-06-27 07:50:35 -0300

    Update midird3tonm.m
    Fixed hanging-out by tables with a single cell.

--- midird3tonm.m
+++ midird3tonm.m
@@ -8,9 +8,10 @@ function midird3tonm = midird3tonm(arquivo)
 %   midired3tonm(filename)
 % Example
 %   midired3tonm('mozart.mid')
-% Created in february 2009, under MATLAB 7.0 (PC).
-% © Part of PARSEMAT - Parseme Toolbox Software Package,    
-% Copyright © 2003, 2005, 2007, 2009, Pauxy Gentil Nunes Filho -
+% Created in February 2009, under MATLAB 7.0 (PC).
+% Revised in Une 2024, under MATLAB 2022a (Mac OS).
+% Â© Part of PARSEMAT - Parseme Toolbox Software Package,    
+% Copyright Â© 2003, 2005, 2007, 2009, Pauxy Gentil Nunes Filho -
 % pauxygnunes@gmail.com
 % See License.txt
 % ========== Getting the info from the MIDI file using midird3.m
@@ -18,18 +19,24 @@ dados = midird3(arquivo);
 sizearquivo = size (dados, 2);
 tab = [];
 tabmt = [];
-    for f = 2:sizearquivo
-    track = sortrows((dados{f}),2);
-    tab = [tab; track];
+    if size(dados,2)>1
+        for f = 2:sizearquivo
+        track = sortrows((dados{f}),2);
+        tab = [tab; track];
+        end
+    else
+        track = sortrows((dados{1}),2);
+        tab = [tab; track];
     end
+maxdur = max(tab(:,4));
 % ========= Mapping between tables (midird3 -> note matrix)
 % tabmt(:,1) = [(tab(2:end,2))-(tab(1:end-1,2)); 0];
-tabmt(:,1) = (tab(:,2))/1024;
-tabmt(:,2) = (tab(:,4))/1024;
+tabmt(:,1) = (tab(:,2))/maxdur*2;
+tabmt(:,2) = (tab(:,4))/maxdur*2;
 tabmt(:,3) = tab(:,6)+1;
 tabmt(:,4) = tab(:,3);
 tabmt(:,5) = tab(:,5);
-tabmt(:,6) = (tab(:,2))/2048;
-tabmt(:,7) = (tab(:,4))/2048;
+tabmt(:,6) = (tab(:,2))/maxdur*4;
+tabmt(:,7) = (tab(:,4))/maxdur*4;
 tabmt = sortrows(tabmt, 1);
 midird3tonm = tabmt;
